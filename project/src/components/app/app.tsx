ca2e1c7ce6b630dfec1cfcb5c6ce89c2815fe536
Artem Malyutin <malyutinartem92@gmail.com>
2022-10-24 18:27:42 +0300

    1. В компоненте App подключен компонент главной страницы MainPage. 2. Созданы заготовки компонентов всех страниц приложения согласно ТЗ и данных из разметки.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+  title: string;
+  genre: string;
+  releaseDate: string;
+}
+
+function App(props: AppProps): JSX.Element {
+  return <MainPage title={ props.title } genre={ props.genre } releaseDate={ props.releaseDate }/>;
 }
 
 export default App;
