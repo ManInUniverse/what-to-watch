486be9834c8f8d81cb17f9129063f4a1923fb9c0
Artem Malyutin <malyutinartem92@gmail.com>
2022-11-24 17:01:17 +0300

    Изменена логика отрисовки отзывов.

--- project/src/pages/film-page-reviews/film-page-reviews.tsx
+++ project/src/pages/film-page-reviews/film-page-reviews.tsx
@@ -10,10 +10,10 @@ function FilmPageReviews(props: FilmPageReviewsProps): JSX.Element {
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
-        { props.reviews.filter((_,index) => index % 2 === 0).map((review) => <Review review={ review } key={ review.id } />) }
+        { props.reviews.slice(props.reviews.length / 2).map((review) => <Review review={ review } key={ review.id } />) }
       </div>
       <div className="film-card__reviews-col">
-        { props.reviews.filter((_,index) => index % 2 !== 0).map((review) => <Review review={ review } key={ review.id } />) }
+        { props.reviews.slice(0, props.reviews.length / 2).map((review) => <Review review={ review } key={ review.id } />) }
       </div>
     </div>
   );
